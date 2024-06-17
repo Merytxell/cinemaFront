0134ee29d0496c330ed83d6b061af082fee098aa
AvilaC <avila.claire@gmail.com>
2024-06-17 14:02:42 +0200

    correction affichage séances

--- src/app/model/movie.model.ts
+++ src/app/model/movie.model.ts
@@ -1,16 +1,18 @@
+import { Cinema } from "./cinema.model";
 import { ShowTime } from "./showTimes.model";
 
 export class Movie {
   id : number;
   name : string;
   showTimes : ShowTime[];
+  cinema : Cinema;
 
 
-
-    constructor(id : number, name: string, showTimes : ShowTime []){
+    constructor(id : number, name: string, showTimes : ShowTime [], cinema: Cinema){
         this.id=id;
         this.name=name;
         this.showTimes=showTimes;
+        this.cinema = cinema;
  
     }
 }
