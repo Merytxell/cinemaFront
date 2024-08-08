8973ab2ef948a667e1783ed112a822f4b5eeac4b
AvilaC <avila.claire@gmail.com>
2024-08-08 10:54:20 +0200

    test cart

--- src/app/components/cart/cart.component.spec.ts
+++ src/app/components/cart/cart.component.spec.ts
@@ -4,38 +4,42 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CartComponent } from './cart.component';
 import { AuthService } from 'src/app/services/auth.service';
 import { ApiService } from 'src/app/services/api.service';
-import { Router } from '@angular/router';
 import { ShowTime } from 'src/app/model/showTimes.model';
+import { CartService } from 'src/app/services/cart.service';
 
 describe('CartComponent', () => {
   let component: CartComponent;
   let fixture: ComponentFixture<CartComponent>;
- 
+  let cartService: jasmine.SpyObj<CartService>;
 
   beforeEach(async () => {
+ 
+    cartService = jasmine.createSpyObj('CartService', ['getTotalPrice', 'addShowTime', 'getCart']);
+
+    cartService.getCart.and.returnValue(new Map<number, ShowTime>());
+
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
         HttpClientTestingModule
       ],
       declarations: [ CartComponent ],
-      
       providers: [
-        {provide : AuthService, useValue : jasmine.createSpyObj('CartService', ['getTotalPrice'])},
-        {provide : ApiService,  useValue : jasmine.createSpyObj('AuthService', ['isUser'])}
+        { provide: CartService, useValue: cartService },
+        { provide: ApiService, useValue: jasmine.createSpyObj('AuthService', ['isUser']) }
       ]
     })
     .compileComponents();
-
-
-
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CartComponent);
     component = fixture.componentInstance;
+
+   
+    component.cart = cartService.getCart();
+
     fixture.detectChanges();
-    
   });
 
   it('should create', () => {
@@ -45,10 +49,18 @@ describe('CartComponent', () => {
   it('should calculate the total price', () => {
     const showTime1: ShowTime = { id: 1, movie: {} as any, hour: '', price: 10, quantity: 2 };
     const showTime2: ShowTime = { id: 2, movie: {} as any, hour: '', price: 20, quantity: 1 };
+
     component.cart.set(showTime1.id, showTime1);
     component.cart.set(showTime2.id, showTime2);
+
     const totalPrice = component.getTotalPrice();
     expect(totalPrice).toBe(40);
-});
+  });
 
+  it('should add a show time', () => {
+    const showTime1: ShowTime = { id: 1, movie: {} as any, hour: '', price: 10, quantity: 1 };
+    component.addShowTime(showTime1);
+    expect(cartService.addShowTime).toHaveBeenCalledWith(showTime1);
+    expect(cartService.getCart).toHaveBeenCalled();
+  });
 });
